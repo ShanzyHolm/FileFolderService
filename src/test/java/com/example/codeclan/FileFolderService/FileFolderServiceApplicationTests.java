62845869edc34dff0848f92200ed5dbc92021064
Shannon Holmes <snholmes13@gmail.com>
2018-10-10 01:02:04 +0100

    Add start to Test file - INCOMPLETE

--- src/test/java/com/example/codeclan/FileFolderService/FileFolderServiceApplicationTests.java
+++ src/test/java/com/example/codeclan/FileFolderService/FileFolderServiceApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example.codeclan.FileFolderService;
 
+import com.example.codeclan.FileFolderService.models.User;
+import com.example.codeclan.FileFolderService.repositories.FileRepository;
+import com.example.codeclan.FileFolderService.repositories.FolderRepository;
+import com.example.codeclan.FileFolderService.repositories.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,28 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class FileFolderServiceApplicationTests {
 
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	FolderRepository folderRepository;
+
+	@Autowired
+	FileRepository fileRepository;
+
+
+
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public  void canCreateUser(){
+		User lenny = new User("Lenny");
+		userRepository,
+	}
+
+	@Test
+
 
 }
