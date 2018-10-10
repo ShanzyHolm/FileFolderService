c12166f31452753fc28fb4d5d2b3376f4759bf2a
Shannon Holmes <snholmes13@gmail.com>
2018-10-10 01:30:14 +0100

    Update and Add to Test and are now passing.  Still not completed the Restful Routes

--- src/test/java/com/example/codeclan/FileFolderService/FileFolderServiceApplicationTests.java
+++ src/test/java/com/example/codeclan/FileFolderService/FileFolderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.codeclan.FileFolderService;
 
+import com.example.codeclan.FileFolderService.models.File;
 import com.example.codeclan.FileFolderService.models.Folder;
 import com.example.codeclan.FileFolderService.models.User;
 import com.example.codeclan.FileFolderService.repositories.FileRepository;
@@ -38,6 +39,21 @@ public class FileFolderServiceApplicationTests {
         Folder week4 = new Folder("Katas", lenny);
         folderRepository.save(week4);
 	}
+
+    @Test
+    public  void canCreateFolderAndFiles(){
+        User lenny = new User("Lenny");
+        userRepository.save(lenny);
+
+        Folder week4 = new Folder("Katas", lenny);
+        folderRepository.save(week4);
+
+        File file1 = new File("Coding For Cats", "ppt", 2.0, week4);
+        fileRepository.save(file1);
+
+        File file2 = new File("Hairball Database", "java", 1.3, week4);
+        fileRepository.save(file2);
+    }
 
 
 }
